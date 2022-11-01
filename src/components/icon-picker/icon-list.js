0375ef59252cedc2c402faa7136f482cac204c34
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-11-01 11:41:48 +0100

    added pagination

--- src/components/icon-picker/icon-list.js
+++ src/components/icon-picker/icon-list.js
@@ -8,6 +8,7 @@ import classnames from 'classnames';
  */
 import { __, _n } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
+import { useState } from '@wordpress/element';
  
 export default function IconList( props ) {
 	const {
@@ -18,6 +19,7 @@ export default function IconList( props ) {
 		showIconNames,
 		iconSize,
 		onClose,
+		iconsPerPage = 100,
 	} = props;
 
 	function updateIconName( name, library, svg ) {
@@ -29,42 +31,61 @@ export default function IconList( props ) {
 		onClose( false );
 	}
 
+	// State Hooks.
+	const [ currentPage, setCurrentPage ] = useState( 0 );
+	const [ iconsLimit, setIconsLimit ] = useState( iconsPerPage );
+
+	let selectedIcons = [];
 	let renderedIcons = [];
 
 	// Fetch all icons if no library is selected.
 	if ( currentLibrary === '__all' ) {
-		renderedIcons = filteredIcons;
+		selectedIcons = filteredIcons;
 	} else {
 		// Fetch icons from current library.
-		renderedIcons = filteredIcons.filter( icon => currentLibrary === icon?.library );
+		selectedIcons = filteredIcons.filter( icon => currentLibrary === icon?.library );
 	}
 
+	// Do not show all icons at once.
+	renderedIcons = selectedIcons.slice(currentPage * iconsPerPage, iconsLimit);
+
 	return (
-		<div
-			className={ classnames( 'tz-icon-list', {
-				'show-icon-names': showIconNames,
-			} ) }
-		>
-			{ renderedIcons.map( ( icon ) => {
-				return (
-					<Button
-						key={ `icon-${ icon.library }-${ icon.name }` }
-						className={ classnames( 'tz-icon-list__item', {
-							'is-active': icon.name === attributes?.iconName && icon.library === attributes?.iconLibrary,
-						} ) }
-						onClick={ () => updateIconName( icon.name, icon.library, icon.icon ) }
-					>
-						<span className="tz-icon-list__item-icon" style={ { width: `${iconSize}px`, height: `${iconSize}px` } }>
-							{ icon.icon }
-						</span>
-						{ showIconNames && (
-							<span className="tz-icon-list__item-name">
-								{ icon.name }
+		<>
+			<div
+				className={ classnames( 'tz-icon-list', {
+					'show-icon-names': showIconNames,
+				} ) }
+			>
+				{ renderedIcons.map( ( icon ) => {
+					return (
+						<Button
+							key={ `icon-${ icon.library }-${ icon.name }` }
+							className={ classnames( 'tz-icon-list__item', {
+								'is-active': icon.name === attributes?.iconName && icon.library === attributes?.iconLibrary,
+							} ) }
+							onClick={ () => updateIconName( icon.name, icon.library, icon.icon ) }
+						>
+							<span className="tz-icon-list__item-icon" style={ { width: `${iconSize}px`, height: `${iconSize}px` } }>
+								{ icon.icon }
 							</span>
-						) }
-					</Button>
-				);
-			} ) }
-		</div>
+							{ showIconNames && (
+								<span className="tz-icon-list__item-name">
+									{ icon.name }
+								</span>
+							) }
+						</Button>
+					);
+				} ) }
+			</div>
+
+			{ ( selectedIcons.length > iconsLimit ) && (
+				<Button
+					isPrimary
+					onClick={ () => setIconsLimit( iconsLimit + iconsPerPage ) }
+				>
+					{ __( 'Show more' ) }
+				</Button>
+			) }
+		</>
 	);
 }
