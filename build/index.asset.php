743119aed309a5d1ce763f7442015286a4a69037
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-11-09 14:43:45 +0100

    run build

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-preferences', 'wp-primitives'), 'version' => 'd76a51e503f7ccdc2405');
